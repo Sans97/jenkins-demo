35fb20c7ece5dd31dd6303a6ee57bce981740522
Sans97 <30297635+Sans97@users.noreply.github.com>
2026-01-13 10:33:34 +0530

    new test build

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "=============================="
 echo "Jenkins Build Triggered!"
-echo "testing build"
+echo "testing build 1"
 echo "Build Time: $(date)"
 echo "Host: $(hostname)"
 echo "=============================="
